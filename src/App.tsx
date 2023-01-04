332fe42889caa622e2751c0b215c5ae43b0899b1
Angela Ivani <angela.ivani@A-M000069.local>
2023-01-05 01:07:06 +0700

    add buggy page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ErrorBoundary from './ErrorBoundary'
+import { Link } from "react-router-dom";
+
 import Landing from './pages/Landing'
 import AddProduct from './pages/AddProduct'
 import ProductList from './pages/ProductList'
@@ -11,18 +12,23 @@ function App() {
 
 
   return (
-    <div className="App">
-      <ErrorBoundary>
-        <h1>hello</h1>
+    <div className="App">              
         <BrowserRouter>
+        <Link to="/" style={{ marginRight: '15px' }}>Landing Page</Link>
+        <Link to="/product/list" style={{ marginRight: '15px' }}>Product List</Link>
+        <Link to="/product/add" style={{ marginRight: '15px' }}>Product Add</Link>
+        <Link to="/buggy-page" style={{ marginRight: '15px' }}>Buggy Page</Link>
+        <br />
+
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/product/add" element={<AddProduct />} />
-          <Route path="/product/list" element={<ProductList />} />
+          <Route path="/product/list" element={<ProductList />} />          
           <Route path="/buggy-page" element={<BuggyPage />} />
+        
+          
         </Routes>
-        </BrowserRouter>
-      </ErrorBoundary>      
+        </BrowserRouter>      
     </div>
   );
 }
--- src/pages/BuggyPage/BuggyPage.tsx
+++ src/pages/BuggyPage/BuggyPage.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { Button } from '@mui/material'
+import ErrorBoundary from '../../ErrorBoundary'
+
 
 const BuggyPage = () => {
   const [counter, setCounter] = useState(0)
@@ -11,14 +13,13 @@ const BuggyPage = () => {
   if(counter === 5) {
     throw new Error('error')
   }
-  
+
   return (
-    <>
+    <ErrorBoundary>
       <Button onClick={handleAddCounter}>Add counter</Button>
       <p>Current counter {counter}</p>
       <p>when counter reach 5, error will be thrown</p>
-    </>
-    
+    </ErrorBoundary>    
   )
 }
 export default BuggyPage
